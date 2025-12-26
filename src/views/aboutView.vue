341bb637a15094f6bd380f3970b6ca4a7a6eb6e3
Korotun <jo.biliec23@gmail.com>
2025-12-26 21:29:05 +0000

    create about page

--- src/views/aboutView.vue
+++ src/views/aboutView.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>About</h1>
+  <div class="container is-max-tablet mt-5">
+    <div class="card">
+      <div class="card-content">
+        <h1 class="title">About</h1>
+        <div class="content">
+          <p>
+            Todo List App is a simple todo list application built with Vue 3 and
+            Vite.
+          </p>
+          <p><strong>Author:</strong> Jordan Biliec</p>
+          <p><strong>Version:</strong> 0.0.1</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
